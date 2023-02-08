4d1b2c40d2ab4e6132cfdd445a6c1746870bb929
SrushtiShah26 <srushtisanskartechnolab13@gmail.com>
2023-02-08 11:14:39 +0545

    Products Type

---
+++ khetan_app/khetan_app/doctype/products_type/test_products_type.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2023, srushti shah and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestProductsType(FrappeTestCase):
+	pass
