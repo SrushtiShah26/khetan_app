3cb8ff89e139e085bfc050c98e632ab4a079690b
SrushtiShah26 <srushtisanskartechnolab13@gmail.com>
2023-02-08 12:22:30 +0545

    gatepass, gatepass_item and item_type

---
+++ khetan_app/khetan_app/doctype/item_type/item_type.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2023, srushti shah and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Item Type', {
+	// refresh: function(frm) {
+
+	// }
+});
